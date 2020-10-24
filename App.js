ae9238623d4c3d31f91a8bc1eb850ed86f5e298e
sqrmyy <dn41.ra20adtdjw@gmail.com>
2020-10-24 10:05:00 +0900

    add ChangeName

--- App.js
+++ App.js
@@ -2,19 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button
-        title='Click Me'
-        onPress={() => {
-          alert('アラート表示');
-          console.log('コンソールログ');
-        }}
-      />
-    </View>
-  );
+export default class App extends React.Component {
+  //stateを定義
+  state = {
+    name: 'hoge',
+    age: 33,
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Button
+          title='Click Me'
+          onPress={() => {
+            alert('アラート表示');
+            console.log('コンソールログ');
+          }}
+        />
+        <Text>こんにちは。{this.state.name}さん。</Text>
+        <Button title='Change Name' onPress={this.changeName} />
+      </View>
+    );
+  }
+  //name変更
+  changeName = () => {
+    this.setState({
+      name: 'Foo',
+    });
+  };
 }
 
 const styles = StyleSheet.create({
